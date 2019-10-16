30c6c701e2308f2eaadd18a9f0086844dbb394e6
User1 <User1@LP-5CD9296CVB.HCLT.CORP.HCL.IN>
2019-10-16 16:00:39 +0530

    files are added

---
+++ src/main/java/com/book/lending/controller/BorrowBookController.java
@@ -0,0 +1,18 @@
+
+package com.book.lending.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author User1
+ *
+ */
+
+@RestController
+public class BorrowBookController {
+
+	
+	
+	
+	
+}
