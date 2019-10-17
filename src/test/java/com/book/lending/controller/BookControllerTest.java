a5539edfd1e921fa856000ce03e9ea0ea6b2b18a
Gurpreet Singh Gohir <gurpreet.gohir@gmail.com>
2019-10-17 16:55:43 +0530

    smell changes

--- src/test/java/com/book/lending/controller/BookControllerTest.java
+++ src/test/java/com/book/lending/controller/BookControllerTest.java
@@ -19,19 +19,17 @@ import com.book.lending.dto.AddBookResponseDto;
 import com.book.lending.dto.BookDto;
 import com.book.lending.dto.BookListDto;
 import com.book.lending.entity.Book;
-import com.book.lending.exception.BookLendingException;
 import com.book.lending.service.BookService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookControllerTest {
-	
-	
+
 	@InjectMocks
 	BookController bookController;
-	
+
 	@Mock
 	BookService bookService;
-	
+
 	@Test
 	public void testGetBookList() {
 		BookDto bookDto1 = new BookDto();
@@ -39,29 +37,28 @@ public class BookControllerTest {
 		BookDto bookDto2 = new BookDto();
 		bookDto2.setAuthor("Madhu");
 		List<BookDto> list = new ArrayList<>();
-		list.add(bookDto1); list.add(bookDto2);
+		list.add(bookDto1);
+		list.add(bookDto2);
 		BookListDto bookListDto = new BookListDto();
 		bookListDto.setBookDto(list);
 		Mockito.when(bookService.getBookList()).thenReturn(bookListDto);
 		ResponseEntity<BookListDto> obj = bookController.getBookList();
 		assertEquals(obj.getBody().getBookDto().size(), list.size());
-		
+
 	}
 
 	@Test
 	public void testAddBook() {
-		
-	AddBookDto addBookDto = new AddBookDto();
-	addBookDto.setAuthor("Shakes");
-	AddBookResponseDto addBookResponseDto = new AddBookResponseDto();
-	addBookResponseDto.setMessage("ADDED SUCCESSFULLY");
-	Book book = new Book();
-	BeanUtils.copyProperties(addBookDto, book);
+
+		AddBookDto addBookDto = new AddBookDto();
+		addBookDto.setAuthor("Shakes");
+		AddBookResponseDto addBookResponseDto = new AddBookResponseDto();
+		addBookResponseDto.setMessage("ADDED SUCCESSFULLY");
+		Book book = new Book();
+		BeanUtils.copyProperties(addBookDto, book);
 		Mockito.when(bookService.addBook(Mockito.any())).thenReturn(addBookResponseDto);
 		ResponseEntity<AddBookResponseDto> obj = bookController.addBook(addBookDto);
 		assertEquals(addBookResponseDto.getMessage(), obj.getBody().getMessage());
 	}
-	
-	
 
 }
