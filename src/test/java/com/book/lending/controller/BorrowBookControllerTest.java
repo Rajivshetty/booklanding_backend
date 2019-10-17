897e42f1f67a71ab27d4b3ae1e74148399a6605d
User1 <User1@LP-5CD9296CVB.HCLT.CORP.HCL.IN>
2019-10-17 13:54:57 +0530

    test file commited

---
+++ src/test/java/com/book/lending/controller/BorrowBookControllerTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.book.lending.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+
+import com.book.lending.dto.BorrowBookRequestDto;
+import com.book.lending.dto.BorrowBookResponseDto;
+import com.book.lending.entity.Book;
+import com.book.lending.entity.User;
+import com.book.lending.service.BorrowBookService;
+import com.book.lending.util.BookUtil;
+
+/**
+ * @author User1
+ *
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class BorrowBookControllerTest {
+	 
+	@Mock
+	private BorrowBookService borrowBookService;
+	
+	@InjectMocks
+	private BorrowBookController borrowBookController;
+
+	/**
+	 * Test method for {@link com.book.lending.controller.BorrowBookController#issueBook(com.book.lending.dto.BorrowBookRequestDto)}.
+	 */
+	@Test
+	public void testIssueBook() {
+
+		User user = new User();
+		Book book = new Book();
+		user.setUserId(1);
+		book.setBookId(2);
+		
+		BorrowBookRequestDto borrowBookRequestDto=new BorrowBookRequestDto();
+		borrowBookRequestDto.setBookId(2);
+		borrowBookRequestDto.setUserId(1);
+		
+		
+		BorrowBookResponseDto borrowBookResponseDto = new BorrowBookResponseDto();
+		borrowBookResponseDto.setStatusCode(BookUtil.BORROW_STATUS_CODE);
+		borrowBookResponseDto.setMessage(BookUtil.BORROW_STATUS__MESSAGE);
+		borrowBookResponseDto.setBookName("Restfull");
+		borrowBookResponseDto.setAuthor("Bill Burke");
+		borrowBookResponseDto.setLanguage("English");
+	
+		Mockito.when(borrowBookService.issueBookService(book.getBookId(), user.getUserId())).thenReturn(borrowBookResponseDto);
+		
+		ResponseEntity<BorrowBookResponseDto> bookResponseDto=borrowBookController.issueBook(borrowBookRequestDto);
+		
+		assertEquals(borrowBookResponseDto.getStatusCode(),bookResponseDto.getBody().getStatusCode());
+	}
+
+}
---
+++ src/test/java/com/book/lending/service/test/BorrowBookServiceImplTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.book.lending.service.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.book.lending.dto.BorrowBookResponseDto;
+import com.book.lending.entity.Book;
+import com.book.lending.entity.BookLending;
+import com.book.lending.entity.User;
+import com.book.lending.exception.BookLendingException;
+import com.book.lending.repository.BookLendingRepository;
+import com.book.lending.repository.BookRepository;
+import com.book.lending.repository.UserRepository;
+import com.book.lending.service.BorrowBookServiceImpl;
+import com.book.lending.util.BookUtil;
+
+/**
+ * @author User1
+ *
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class BorrowBookServiceImplTest {
+
+	
+	@Mock
+	private BookLendingRepository bookLendingRepository;
+
+	@Mock
+	private BookRepository bookRepository;
+
+	@Mock
+	private UserRepository userRepository;
+	
+	@InjectMocks
+	private BorrowBookServiceImpl borrowBookServiceImpl;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void issueBookServiceTest() {
+
+		User user = new User();
+		Book book = new Book();
+		user.setUserId(1);
+		book.setBookId(2);
+
+		LocalDate today = LocalDate.now();
+		LocalDate nextWeek = today.plusDays(7);
+		BookLending bookLending = new BookLending();
+		bookLending.setBookId(100);
+		bookLending.setUserId(99);
+		bookLending.setIssuedDate(today);
+		bookLending.setSubmissionDate(nextWeek);
+
+		Mockito.when(userRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(user));
+		Mockito.when(bookRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(book));
+
+		Mockito.when(bookLendingRepository.save(bookLending)).thenReturn(null);
+
+		BorrowBookResponseDto borrowBookResponseDto = new BorrowBookResponseDto();
+		borrowBookResponseDto.setStatusCode(BookUtil.BORROW_STATUS_CODE);
+		borrowBookResponseDto.setMessage(BookUtil.BORROW_STATUS__MESSAGE);
+		borrowBookResponseDto.setBookName("Restfull");
+		borrowBookResponseDto.setAuthor("Bill Burke");
+		borrowBookResponseDto.setLanguage("English");
+
+		BorrowBookResponseDto bookResponseDto = borrowBookServiceImpl.issueBookService(bookLending.getBookId(),
+				bookLending.getUserId());
+
+		assertEquals(borrowBookResponseDto.getStatusCode(), bookResponseDto.getStatusCode());
+
+	}
+
+	@Test(expected = BookLendingException.class)
+	public void issueBookServiceTestException() {
+
+		User user = new User();
+		Book book = new Book();
+		user.setUserId(1);
+		book.setBookId(2);
+
+		borrowBookServiceImpl.issueBookService(11, 12); 
+
+	}
+
+}
